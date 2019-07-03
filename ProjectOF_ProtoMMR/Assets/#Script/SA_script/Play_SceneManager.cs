0fd1c2429d66bc792272cc004d20a666ced23978
kimminkyoung <somuon9981@daum.net>
2019-07-03 15:25:34 +0900

    Add_SceneManager
    씬매니저 추가하고 프로그래스 바 추가

--- ProjectOF_ProtoMMR/Assets/#Script/SA_script/Play_SceneManager.cs
+++ ProjectOF_ProtoMMR/Assets/#Script/SA_script/Play_SceneManager.cs
@@ -2,11 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Play_SceneManager : MonoBehaviour
 {
-    public static void NextScene()
+    AsyncOperation asyncOper;
+    bool PlayReady = false;
+    public RectTransform ProgressBar;
+
+    private void Start()
     {
-        SceneManager
+        StartCoroutine(StartLoad());
+    }
+
+    public void Ready()
+    {
+        PlayReady = true;
+    }
+
+    IEnumerator StartLoad()
+    {
+        asyncOper = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        asyncOper.allowSceneActivation = false;
+
+        while (!asyncOper.isDone)
+        {
+            yield return null;
+            if (asyncOper.progress >= 0.9f)
+            {
+                if (ProgressBar != null)
+                {
+                    ProgressBar.localScale = Vector3.one;
+                    ProgressBar.gameObject.GetComponent<Image>().color = Color.cyan;
+                }
+                if (PlayReady == true)
+                {
+                    asyncOper.allowSceneActivation = true;
+                }
+            }
+            else if (ProgressBar != null)
+            {
+                 ProgressBar.localScale = new Vector3(asyncOper.progress, 1, 1);
+            }
+        }
     }
 }
