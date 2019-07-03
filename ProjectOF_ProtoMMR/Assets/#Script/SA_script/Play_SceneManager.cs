544ed12ce6a9b69a52e944ba51f9334c91d40ab0
kimminkyoung <somuon9981@daum.net>
2019-07-03 14:49:21 +0900

    Add_ProtoTypeAsset
    프로토타입용 에셋추가

---
+++ ProjectOF_ProtoMMR/Assets/#Script/SA_script/Play_SceneManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Play_SceneManager : MonoBehaviour
+{
+    public static void NextScene()
+    {
+        SceneManager
+    }
+}
