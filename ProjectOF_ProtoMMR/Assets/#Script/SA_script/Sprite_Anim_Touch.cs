624277d93d6928b0a6eb35100a91aa1f97b82db7
kimminkyoung <somuon9981@daum.net>
2019-07-16 15:01:33 +0900

    Revising_AnimTouchControl
    애니메이션 터치 스크립트
    > spine touch(멀티터치가능)
    > sprite touch(스프라이트 개수 자유)
    > sprite anim touch

---
+++ ProjectOF_ProtoMMR/Assets/#Script/SA_script/Sprite_Anim_Touch.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sprite_Anim_Touch : MonoBehaviour
+{
+    Animator anim;
+    public string animName;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.speed = 0;
+    }
+    private void OnMouseUp()
+    {
+        anim.speed = 1;
+        anim.Play(animName);
+    }
+}
---
+++ ProjectOF_ProtoMMR/Assets/#Script/SA_script/Sprite_Touch.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Sprite_Touch : MonoBehaviour
+{
+    public List<Sprite> mySprite = new List<Sprite>();
+    int SpriteIndex = 0;
+
+    private void OnMouseUp()
+    {
+        if (SpriteIndex < mySprite.Count)
+        {
+            GetComponent<Image>().sprite = mySprite[SpriteIndex];
+            SpriteIndex++;
+        }
+        else
+            print("끝");
+    }
+}
