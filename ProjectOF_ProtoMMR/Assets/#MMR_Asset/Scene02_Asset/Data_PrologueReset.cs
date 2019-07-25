81437f0d7cfb46b3f94f9ab9261c3c94524147fc
kimminkyoung <somuon9981@daum.net>
2019-07-25 15:33:28 +0900

    Revising

---
+++ ProjectOF_ProtoMMR/Assets/#MMR_Asset/Scene02_Asset/Data_PrologueReset.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data_PrologueReset : MonoBehaviour
+{
+    public void ResetPrologue()
+    {
+        PlayerPrefs.DeleteKey("SavePrologue");
+        print("SavePrologue의 키값과 데이터를 지웠습니다.");
+    }
+}
